d10dc0a15ed8d997d3a2477e2eb56c17babd6c5a
hreidar17 <hreidar17@ru.is>
2019-03-20 15:59:11 +0000

    first push from react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SocketContext from './contexts/SocketContext';
+import Dummy from './components/Dummy/Dummy'
 import './App.css';
 
 class App extends Component {
+
+  getName(){
+    var person = prompt("Please enter your name. \nMust be shorter than 21 characters", "Anonymous");
+      if(person == null || person.length > 21){
+        alert("Invalid Name!");
+        return this.getName();
+      }
+      else {
+        return person;
+      }
+  }
+
+
+  componentDidMount(){
+    const { socket } = this.context;
+    console.log(socket);
+
+    socket.emit("adduser", this.getName(), function(available){
+      if (available){
+        console.log("user added!");
+      }
+      else{
+        console.log("something went wrong");
+        alert("Name already taken :(");
+      }
+    });
+  }
+/*
+socket.emit("adduser", "GnoMe", function(available){
+  if (available){
+    console.log("user added!");
+  }
+  else{
+    console.log("something went wrong");
+  }
+});
+*/
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Dummy />
       </div>
     );
   }
 }
+
+App.contextType = SocketContext;
 
 export default App;
---
+++ src/components/Dummy/Dummy.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dummy extends Component {
+  render() {
+    return (
+      <h1>CHAT WINDOES GOES HERE!</h1>
+    );
+  }
+}
+
+export default Dummy;
